4fdf25d535a47636133ffa1db7654f9b0176dbef
EServetti <152347701+EServetti@users.noreply.github.com>
2024-03-17 17:28:28 -0300

    Agregue los 10 productos

--- memory/ProductManager.js
+++ memory/ProductManager.js
@@ -68,46 +68,86 @@ class ProductManager {
 //Creo una constante que invoca a la clase ProductManager
 const producto = new ProductManager();
 //Invoco al metodo create de la clase
-
 producto.create({
-  title: "Alfajor",
-  photo: "alfajor.jpg",
+  title: "Tatin",
+  photo: "https://masonlineprod.vtexassets.com/arquivos/ids/246188/Alfajor-Simple-Tatin-Negro-33g-1-32815.jpg?v=637883553702800000",
   category: "comida",
   price: 250,
   stock: 25,
 });
-//Al faltarle una propiedad (title) no creara este product y devolvera un log
+//producto 2
 producto.create({
-  //title : 'Paquete de harina',
-  photo: "harina.jpg",
-  category: "comida",
-  price: 500,
+  title: "Tarta de Frutas",
+  photo: "https://placeralplato.com/files/2015/05/Receta-de-tarta-de-frutas-640x480.jpg?width=1200&enable=upscale",
+  category: "repostería",
+  price: 350,
   stock: 20,
 });
-
+//producto 3
 producto.create({
-  title: "Peine",
-  photo: "peine.jpg",
-  category: "Higiene",
-  price: 400,
-  stock: 10,
-});
-
-producto.create({
-  title: "Cepillo de dientes",
-  photo: "cepillo.jpg",
-  category: "Higiene",
-  price: 400,
+  title: "Auriculares Inalámbricos",
+  photo: "https://images-na.ssl-images-amazon.com/images/I/71IvLIxaLCL._AC_SL1500_.jpg",
+  category: "tecnología",
+  price: 1200,
   stock: 15,
 });
 
 producto.create({
-  title: "Gorra",
-  photo: "gorra.jpg",
-  category: "Indumentaria",
-  price: 1000,
-  stock: 5,
+  title: "Vestido de Noche",
+  photo: "https://thumbs.dreamstime.com/b/vestido-de-noche-en-un-maniqu%C3%AD-18692928.jpg",
+  category: "moda",
+  price: 1800,
+  stock: 10,
 });
+//producto 5
+producto.create({
+  title: "Set de Pinceles de Maquillaje",
+  photo: "https://tienda.artepierrot.cl/wp-content/uploads/2020/10/Pinceles-Social-AP2-600x600.png",
+  category: "belleza",
+  price: 500,
+  stock: 30,
+});
+//producto 6
+producto.create({
+  title: "Balón de Fútbol",
+  photo: "https://img.planetafobal.com/2021/07/uefa-champions-league-pelota-oficial-2021-2022-pyrostorm-yu.jpg",
+  category: "deportes",
+  price: 800,
+  stock: 25,
+});
+//producto 7
+producto.create({
+  title: "Mochila Escolar",
+  photo: "https://images-na.ssl-images-amazon.com/images/I/71fU3CrTNZL._AC_SL1500_.jpg",
+  category: "accesorios",
+  price: 600,
+  stock: 18,
+});
+//producto 8
+producto.create({
+  title: "Juego de Mesa de Estrategia",
+  photo: "https://tse4.mm.bing.net/th?id=OIP.rvtOjGzBffRXMx1Pyb-PegHaE9&pid=Api&P=0&h=180",
+  category: "entretenimiento",
+  price: 900,
+  stock: 12,
+});
+//producto 9
+producto.create({
+  title: "Botines de Fútbol",
+  photo: "https://www.deportesapalategui.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/a/o/ao3258-400-phcfh001-1000.jpeg",
+  category: "deportes",
+  price: 1500,
+  stock: 8,
+});
+//producto 10
+producto.create({
+  title: "Juego de Sábanas de Algodón",
+  photo: "https://tse4.mm.bing.net/th?id=OIP.ZdaYwifhD9W2xNU9xAa9MQHaHa&pid=Api&P=0&h=180",
+  category: "hogar",
+  price: 1000,
+  stock: 20,
+});
+
 //devuelve todos los productos
 console.log(producto.read());
 //devuelve un producto especifico segun el id
