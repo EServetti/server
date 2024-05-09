b73f68462357874b118c69165ad260473b5f8929
unknown <eservetti2018@gmail.com>
2024-05-09 11:44:18 -0300

    Agregada la propiedad name a user y modificados los users actuales

--- src/data/mongo/models/users.model.js
+++ src/data/mongo/models/users.model.js
@@ -6,6 +6,7 @@ import { type } from "os";
 const collection =  "users"
 
 const schema = new Schema ({
+  name: {type: String, required: true, index: true},
   photo: {type: String, default: "/img/defaultUser.webp"},
   email: {type: String, required: true, unique: true, index: true},
   password:{type: String, required: true},
