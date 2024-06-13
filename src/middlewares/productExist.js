503ac164710de43052c03ba580bc634cf76d577b
unknown <eservetti2018@gmail.com>
2024-06-13 20:57:31 -0300

    Actualizados los middlewares a las persistencias cambiables

--- src/middlewares/productExist.js
+++ src/middlewares/productExist.js
@@ -1,17 +1,9 @@
-import productManager from "../DAO/mongo/managers/ProductManager.db.js";
+import { readService } from "../service/api/products.api.service.js";
 async function exist (req, res, next) {
 try { 
     const { title } = req.body;
-    const all = await productManager.read();
+    const all = await readService();
     const exist = all.some((product) => product.title === title);
-    console.log(req.body);
-    //console.log(all);
-    /*all.some((product)=> {
-        console.log(title)
-        console.log(product.title)
-        console.log(title===product.title)
-        return title===product.title
-     })*/
     if (exist) {
         res.status(400).json({
             statusCode: 400,
