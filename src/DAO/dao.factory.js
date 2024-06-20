62b9239e97babada30db4ed20430a0a84164bac1
unknown <eservetti2018@gmail.com>
2024-06-19 23:50:14 -0300

    Agregado patron factory

--- src/DAO/dao.factory.js
+++ src/DAO/dao.factory.js
@@ -1,58 +1,81 @@
 import argsUtil from "../utils/args.util.js";
 import dbConnect from "../utils/DbConnection.js";
 
-let dao = {};
 const persistence = argsUtil.pers;
+let dao = {};
 
-switch (persistence) {
-  case "memory":
-    console.log("Connected to memory");
-    const { default: userManagerMem } = await import(
-      "../DAO/memory/UserManager.js"
-    );
-    const { default: productManagerMem } = await import(
-      "../DAO/memory/ProductManager.js"
-    );
-    const { default: cartsManagerMem } = await import(
-      "../DAO/memory/CartsManager.js"
-    );
-    dao = {
-      userManager: userManagerMem,
-      productManager: productManagerMem,
-      cartManager: cartsManagerMem,
-    };
-    break;
-  case "fs":
-    console.log("Connected to files system");
-    const { default: userManagerFs } = await import("../DAO/fs/UserManager.js");
-    const { default: productManagerFs } = await import(
-      "../DAO/fs/ProductManager.js"
-    );
-    const { default: cartsManagerFs } = await import(
-      "../DAO/fs/CartManager.js"
-    );
-    dao = {
-      userManager: userManagerFs,
-      productManager: productManagerFs,
-      cartManager: cartsManagerFs,
-    };
-    break;
-  default:
-    console.log("Connected to database");
-    await dbConnect()
-    const { default: userManagerDb } = await import("../DAO/mongo/managers/UserManager.db.js");
-    const { default: productManagerDb } = await import(
-      "../DAO/mongo/managers/ProductManager.db.js"
-    );
-    const { default: cartsManagerDb } = await import(
-      "../DAO/mongo/managers/CartManager.db.js"
-    );
-    dao = {
-      userManager: userManagerDb,
-      productManager: productManagerDb,
-      cartManager: cartsManagerDb,
-    };
-    break;
+async function switchPersistence() {
+  try {
+    switch (persistence) {
+      case "memory":
+        try {
+          console.log("connected to memory");
+          const { default: productsManagerMem } = await import(
+            "./memory/ProductManager.js"
+          );
+          const { default: cartsManagerMem } = await import(
+            "./memory/CartsManager.js"
+          );
+          const { default: usersManagerMem } = await import(
+            "./memory/UserManager.js"
+          );
+          dao = {
+            users: usersManagerMem,
+            products: productsManagerMem,
+            carts: cartsManagerMem,
+          };
+        } catch (error) {
+          console.error("Error loading memory managers:", error);
+        }
+        break;
+      case "fs":
+        try {
+          console.log("connected to file system");
+          const { default: productsManagerFs } = await import(
+            "./fs/ProductManager.fs.js"
+          );
+          const { default: cartsManagerFs } = await import(
+            "./fs/CartManager.fs.js"
+          );
+          const { default: usersManagerFs } = await import(
+            "./fs/UserManager.fs.js"
+          );
+          dao = {
+            users: usersManagerFs,
+            products: productsManagerFs,
+            carts: cartsManagerFs,
+          };
+        } catch (error) {
+          console.error("Error loading file system managers:", error);
+        }
+        break;
+      default:
+        try {
+          console.log("connected to database");
+          await dbConnect();
+          const { default: productsManagerMongo } = await import(
+            "./mongo/managers/ProductManager.db.js"
+          );
+          const { default: cartsManagerMongo } = await import(
+            "./mongo/managers/CartManager.db.js"
+          );
+          const { default: usersManagerMongo } = await import(
+            "./mongo/managers/UserManager.db.js"
+          );
+          dao = {
+            users: usersManagerMongo,
+            products: productsManagerMongo,
+            carts: cartsManagerMongo,
+          };
+        } catch (error) {
+          console.error("Error loading database managers:", error);
+        }
+        break;
+    }
+  } catch (error) {
+    console.error("Error in switchPersistence:", error);
+  }
 }
 
+await switchPersistence();
 export default dao;
