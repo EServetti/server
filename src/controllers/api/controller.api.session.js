405588285f8192be9887e37ef58d71e6a14fe5fe
unknown <eservetti2018@gmail.com>
2024-06-25 16:03:41 -0300

    Agregado endpoint para verificar el mail

--- src/controllers/api/controller.api.session.js
+++ src/controllers/api/controller.api.session.js
@@ -1,4 +1,4 @@
-
+import { readByEmailService, updateService } from "../../service/users.api.service.js";
 
 async function register (req, res, next) {
     try {
@@ -25,6 +25,25 @@ async function register (req, res, next) {
       return next(error);
     }
   }
+
+  async function authenticate (req, res, next) {
+    try {
+      const {email, verifyCode } = req.query
+      const one = await readByEmailService(email)
+      const verified = verifyCode === one.verifyCode
+      if(!verified) {
+        return res.error400("Invalid credentials!")
+      } else {
+        await updateService(one._id, {
+          verify: true
+        })
+        const message = `The account ${one.email} has been authenticated!`
+        return res.render("verify", {title: "VERIFY", message})
+      }
+    } catch (error) {
+      return next(error)
+    }
+  }
   
   async function signout (req, res, next) {
     try {
@@ -36,4 +55,4 @@ async function register (req, res, next) {
     }
   }
 
-export {register, login, data, signout}
+export {register, login, data, authenticate, signout}
